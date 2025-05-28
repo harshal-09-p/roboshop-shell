7bdcbcd66bf6ecd8525b9d23566c4cd033779f47
harshal-09-p <harshal.phadatare@gmail.com>
2025-05-28 16:22:51 +0530

    mongodb sed changes

--- mongoDB.sh
+++ mongoDB.sh
@@ -5,6 +5,6 @@ dnf install mongodb-org -y
 systemctl enable mongod 
 systemctl start mongod 
 
-sed -i -e 's|127.0.0.1|0.0.0.0|g'
+sed -i -e 's|127.0.0.1|0.0.0.0|g' /etc/mongod.conf
 
 systemctl restart mongod
