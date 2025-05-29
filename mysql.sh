796026193fd17d3420ca02a3007b7d334423c1cb
harshal-09-p <harshal.phadatare@gmail.com>
2025-05-29 18:36:55 +0530

    added if conditiones

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,16 @@
+if [-z "$1" -a -z "$PASS"]; then
+    echo input password is missing, kindly kindly pass the password while executing.
+    exit
+elif [-n "$1"]; then
+    PASSWD=$1
+else [-n "$PASS"]; then
+    PASSWD=${PASS}
+fi  
+
 dnf install mysql-server -y
 
 systemctl enable mysqld
 systemctl start mysqld  
 
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass ${PASSWD}
 
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,3 +1,7 @@
+if [-z "$1"]; then
+    echo input password data is missing, kindly pass the password while executing.
+    exit
+fi
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 
 dnf install rabbitmq-server -y
@@ -5,6 +9,6 @@ dnf install rabbitmq-server -y
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
 
-rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl add_user roboshop $1
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 
