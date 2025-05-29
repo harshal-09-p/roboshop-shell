3e3d1b497355546444a8386286bee59b1e277f7d
harshal-09-p <harshal.phadatare@gmail.com>
2025-05-29 19:08:44 +0530

    included for loop

--- shipping.sh
+++ shipping.sh
@@ -2,16 +2,19 @@ component_name=shipping
 path=$(pwd)
 source $(pwd)/common.sh
 
+if [ -z "$1" ]; then
+    echo input DB password is missing, kindly pass while executing.
+    exit
+fi
+
 java
 
 systemd_setup
 
 dnf install mysql -y 
 
-mysql -h mysql-dev.harshaldevops.online -uroot -pRoboShop@1 < /app/db/schema.sql
-
-mysql -h mysql-dev.harshaldevops.online -uroot -pRoboShop@1 < /app/db/app-user.sql 
-
-mysql -h mysql-dev.harshaldevops.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+for file in schema app-user master-data; do 
+    mysql -h mysql-dev.harshaldevops.online -uroot -p$1 < /app/db/$file.sql
+done
 
 systemctl restart shipping
